9fdda2bb85e6e0888e42d06132b68d777f96aea6
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-08-24 20:08:18 -0300

    retirado diretorio test

--- djangopythonpro/tests/send_email.py
+++
@@ -1,9 +0,0 @@
-class Enviador:
-    def enviar(self, remtente_address, destinatario, assunto, corpo_email):
-        if "@" not in destinatario:
-            raise EmailInvalido(f"Email {destinatario} é inválido")
-        return destinatario
-
-
-class EmailInvalido(Exception):
-    pass
