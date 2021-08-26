f5d313a2b9e77193a36953018bdd9ed5e9d8488c
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-08-26 13:43:29 -0300

    add debug toolbar

--- djangopythonpro/base/views.py
+++ djangopythonpro/base/views.py
@@ -1,8 +1,19 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("Ola django")
+    return HttpResponse("""
+    <html>
+<head>
+     <meta content='text/html'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Document</title>
+</head>
+<body>
+    <h3>Olá django</h3>
+</body>
+</html>""")
