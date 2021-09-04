36fbe0bd3cf87c2e570bc347b5c102376221c68f
marcos p s viana <marcospaulo.silvaviana@gmail.com>
2021-09-04 19:25:26 -0300

    Developer (#31)
    * testes ok, add banco posgres e videos
    
    * testes ok, add fixtures db com video
    
    * reformating files
    
    * modificado travis yaml
    
    * add git actions
    
    * modificado ga
    
    * modificado ci para branch main
    
    * revisado env-sample
    
    * tests links, add migrate videos
    
    * alterado model de slug para slugfield

--- djangopythonpro/demo/models.py
+++ djangopythonpro/demo/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
 
 class Videos(models.Model):
-    slug = models.CharField(max_length=35)
     titulo = models.CharField(max_length=60)
+    slug = models.SlugField(max_length=35)
     video_id = models.CharField(max_length=35)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def get_absolute_url(self):
+        return reverse("demo:video", args=(self.slug,))
+
+    def __str__(self):
+        return f"Vídeo: {self.slug}"
+
+    class Meta:
+        verbose_name_plural = _("videos")
+
