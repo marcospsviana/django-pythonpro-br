65f2874f6d0ccc7d43ccb46704c82e1a8bc95f75
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-09-04 18:14:28 -0300

    tests links, add migrate videos

--- djangopythonpro/demo/views.py
+++ djangopythonpro/demo/views.py
@@ -1,35 +1,10 @@
 from django.shortcuts import render, get_list_or_404
 from djangopythonpro.demo.models import Videos
 
-# from django.http.response import HttpResponseNotFound
-
-
-# videos_class = [
-#     Videos("renzo", "os fodásticos do python dev pro", "a61p-g0yWts"),
-#     Videos(
-#         "rocha-bruno",
-#         "Coisas que NÃO devemos fazer ao programar em PYTHON - Codeshow",
-#         "p4jWEC7vuKI",
-#     ),
-# ]
-videos_class = [
-    {
-        "slug": "renzo",
-        "titulo": "os fodásticos do python dev pro",
-        "video_id": "a61p-g0yWts",
-    },
-    {
-        "slug": "rocha-bruno",
-        "titulo": "Coisas que NÃO devemos fazer ao programar em PYTHON - Codeshow",
-        "video_id": "p4jWEC7vuKI",
-    },
-]
-
-videos_slugs = {vid["slug"]: vid for vid in videos_class}
-
 
 def indice(request):
-    return render(request, "demonstrativo/indice.html", context={"video": videos_class})
+    video = Videos.objects.order_by('created_at').all()
+    return render(request, "demonstrativo/indice.html", context={"video": video})
 
 
 def video(request, slug):
