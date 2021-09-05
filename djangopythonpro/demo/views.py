91d5518b8272dbfc0c3180af6eb67ba7df27b10b
marcos p s viana <marcospaulo.silvaviana@gmail.com>
2021-09-04 21:26:19 -0300

    Developer (#32)
    * testes ok, add banco posgres e videos
    
    * testes ok, add fixtures db com video
    
    * reformating files
    
    * modificado travis yaml
    
    * add git actions
    
    * modificado ga
    
    * modificado ci para branch main
    
    * revisado env-sample
    
    * tests links, add migrate videos
    
    * alterado model de slug para slugfield
    
    * alterado coverage

--- djangopythonpro/demo/views.py
+++ djangopythonpro/demo/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from djangopythonpro.demo.models import Videos
 
 
 def indice(request):
-    video = Videos.objects.order_by('created_at').all()
+    video = Videos.objects.order_by("created_at").all()
     return render(request, "demonstrativo/indice.html", context={"video": video})
 
 
 def video(request, slug):
-    video_data = get_list_or_404(Videos, slug=slug)
+    video_data = get_object_or_404(Videos, slug=slug)
     return render(request, "demonstrativo/video.html", context={"video": video_data})
